63ab66790bd1da105051fd45fcb3d94a2585a29c
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-05-30 18:45:05 -0500

    finisged projectData array in seeds folder

--- src/seeds/projectsArray.js
+++ src/seeds/projectsArray.js
@@ -1,15 +1,121 @@
 const projects = [
   {
     id: 1,
-    name: "Project 1",
-    description: "This is a description ",
-    technologies: ["list here"],
+    name: "Random Password Generator",
+    description:
+      "A password generator that can generate random passwords based on criteria that the user has selected",
+    technologies: ["JavaScript", "HTML", "CSS"],
     image: "path to image here",
-    repo: "url",
-    demoVideo: "url",
+    repo: "https://github.com/ohSweetWampum/password-generator",
+    deployedLink: "https://ohsweetwampum.github.io/password-generator/",
   },
-
-  // Add all of ypur projects here
+  {
+    id: 2,
+    name: "Weather Dashboard",
+    description:
+      "App displays the current day's weather as well as the 5-day forecast for a particular city. You can search a city and the weather data will be displayed. ",
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "jQuery",
+      "Bootstrap",
+      "OpenWeather API",
+      "Day.js",
+    ],
+    image: "path to image here",
+    repo: "https://github.com/ohSweetWampum/weather-dashboard",
+    deployedLink: "https://ohsweetwampum.github.io/weather-dashboard/",
+  },
+  {
+    id: 3,
+    name: "E-Commerce Back End",
+    description: "This is the back-end of a shopping website ",
+    technologies: [
+      "Express.js",
+      "MySQL2",
+      "Sequelize",
+      "Dotenv",
+      "Insomnia",
+      "Node.js",
+      "JavaScript",
+    ],
+    image: "path to image here",
+    repo: "https://github.com/ohSweetWampum/E-Commerce-Back-End",
+    deployedLink:
+      "https://drive.google.com/file/d/1Fvk_3knhg5utdPcn7OCdCTom6Aj0RylA/view",
+  },
+  {
+    id: 4,
+    name: "Big Sky Lodges",
+    description:
+      "This hotel booking app, built with Node.js, Express, Sequelize, MySQL, and session management, is a full-stack application which offers a secure and user-friendly interface for managing hotel reservations across multiple branches. It enables users to securely register, login, book rooms, view their reservations on a personalized dashboard, and cancel bookings as needed ",
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Node.js",
+      "Express.js",
+      "Sequelize",
+      "MySQL2",
+      "Handlebars.js",
+      "Bcrypt",
+      "Dotenv",
+      "Express-Session",
+      "Bootstrap",
+      "Heroku",
+      "JawsDB",
+      "Day.js",
+      "Express-Handlebars",
+      "Connect-Session-sequalize",
+      "Bootstrap",
+    ],
+    image: "path to image here",
+    repo: "https://github.com/ohSweetWampum/Big-Sky-Lodges",
+    deployedLink: "https://big-sky-lodges.herokuapp.com/",
+  },
+  {
+    id: 5,
+    name: "Savvy Chef",
+    description:
+      "Savvy Chef is a web application that helps you find recipes based on the ingredients you have on hand. With its intuitive interface, you can easily search for ingredients and add them to your list. The app then uses a Spoonacular API to suggest recipes that match your ingredient list. You can also filter the recipe results based on food intolerances, dietary restrictions, price sorting, and cuisines. Savvy chef also uses the CocktailDB API to generate delicious and unique cocktails. ",
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "jQuery",
+      "Bulma",
+      "APIs",
+      "Spoonacular API",
+      "CocktailDB API",
+    ],
+    image: "path to image here",
+    repo: "https://github.com/DidrikLindberg/savvy-chef",
+    deployedLink: "https://didriklindberg.github.io/savvy-chef/",
+  },
+  {
+    id: 6,
+    name: "TowneSquare",
+    description:
+      "TownSquare is a social media application designed for user interactivity. It allows users to create profiles, post thoughts, and respond to others' posts. The most important feature of the app is the ability to manage friend connections, you can add or remove friends. Functionality for updating and deleting posts provides users with control over their content. The application utilizes the power of MongoDB's CRUD operations, while Express.js provides the routing. ",
+    technologies: [
+      "Express.js",
+      "MongoDB",
+      "Mongoose",
+      "Node.js",
+      "JavaScript",
+      "Dotenv",
+      "Bootstrap",
+      "Heroku",
+      "JawsDB",
+      "Insomnia",
+      "Day.js",
+    ],
+    image: "path to image here",
+    repo: " https://github.com/ohSweetWampum/TownSquare",
+    deployedLink:
+      " https://drive.google.com/file/d/1Ext6K_odyTyfKsVvN2OWercw-vLhbpZu/view",
+  },
 ];
 
 export default projects;
