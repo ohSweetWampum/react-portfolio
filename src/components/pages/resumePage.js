f6c88f91c6080b6bfe12fd7aeb25ae08488f5d15
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-05-30 09:46:14 -0500

    created the resumPage.js rough draft

---
+++ src/components/pages/resumePage.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Document, Page } from "react-pdf";
+import resumePDF from "./images/Matthew Gibson Resume 03_28_2022-2.pdf";
+
+const Resume = () => {
+  const skills = ["JavaScript", "React", "Node.js", "Express.js"]; // add more skilss
+
+  return (
+    <div
+      style={{
+        position: "relative",
+        width: "80%",
+        margin: "0 auto",
+        textAlign: "center",
+      }}
+    >
+      <Document
+        file={resumePDF}
+        onLoadSuccess={() => console.log("PDF loaded successfully")}
+      >
+        <Page pageNumber={1} width={window.innerWidth * 0.8} />
+      </Document>
+      <div style={{ margin: "20px auto" }}>
+        <a href={resumePDF} download="resume.pdf">
+          Download Resume
+        </a>
+        <br />
+        <a href={resumePDF} target="_blank" rel="noopener noreferrer">
+          Open Resume in New Tab
+        </a>
+        <br />
+        <button onClick={() => window.print()}>Print Resume</button>
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          backgroundColor: "#f0f0f0",
+          padding: "10px",
+        }}
+      >
+        <h3>Skills</h3>
+        <ul>
+          {skills.map((skill) => (
+            <li key={skill}>{skill}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Resume;
